9b0e4372fe43145977fa35163456e4ff8e013d7f
chemputer <46601333+chemputer@users.noreply.github.com>
2019-01-30 15:09:27 -0500

    Initial commit, created branch chemputer & added my files. Doc link is in readme.

---
+++ README.md
@@ -0,0 +1,7 @@
+# CSC226C 
+
+### Assignment A03
+
+Name: Ben Turner
+
+Google Doc: https://docs.google.com/document/d/18mGsKUgGGTHoyN-BFd13Pg0GE5MTkCofZjxaS1oXSR0/edit?usp=sharing
---
+++ a03-turnerb.py
@@ -0,0 +1,11 @@
+##
+# Ben Turner
+# A03: A Pair of Fully Functional Gitty Psychedelic Robotic Turtles
+# references the assignment doc:
+# https://docs.google.com/document/d/18mGsKUgGGTHoyN-BFd13Pg0GE5MTkCofZjxaS1oXSR0/edit?usp=sharing
+##
+
+import turtles
+from random import randint
+
+# TO
