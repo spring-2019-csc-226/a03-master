140a7b5243479432bf7ad0c3689178967b98c273
millerwi <millerwi@berea.edu>
2019-02-07 20:20:03 -0500

    i hope this is right

---
+++ A02_millerwi.py
@@ -0,0 +1,43 @@
+# William Miller
+import turtle
+
+
+def make_circle(smiley):
+    smiley.setpos(0,-200)
+    smiley.circle(200)
+
+
+def make_other_lines(smiley):
+    smiley.left(90)
+    smiley.forward(400)
+    smiley.backward(200)
+    smiley.right(45)
+    smiley.forward(200)
+    smiley.backward(200)
+    smiley.left(90)
+    smiley.forward(200)
+
+
+def main():
+    wn = turtle.Screen()
+    wn.bgcolor("black")
+    smiley = turtle.Turtle()
+    smiley.color("white")
+    smiley.pensize(10)
+    smiley.shape("circle")
+
+    make_circle(smiley)
+    make_other_lines(smiley)
+    wn.exitonclick()
+
+main()
+
+
+
+
+
+
+
+
+
+
