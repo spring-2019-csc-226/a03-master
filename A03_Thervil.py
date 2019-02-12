9919d5890bddcfd7b04a5fee83d1ac63714ce6f8
thervilw <thervilw@berea.edu>
2019-02-12 12:22:21 -0500

    finished creating shapes

--- A03_Thervil.py
+++ A03_Thervil.py
@@ -8,9 +8,6 @@ willy.setheading(90)
 willy.setpos(0, 0)
 
 jess = turtle
-tess = turtle
-tess.setpos(100, -200)
-tess.pendown()
 
 def function_1():
     """ creates shape
@@ -44,17 +41,50 @@ def function_3():
     """creates house
     :param : turtle object"""
     jess.color("blue")
+    jess.pensize(5)
     jess.penup()
-    jess.setpos(100, -250)
+    jess.setpos(80, -215)
     jess.pendown()
     for j in range(4):
-        pass
+        jess.forward(150)
+        jess.left(90)
 
+def function_4():
+    """creates house
+    :param : turtle object"""
+    jess.color("red")
+    jess.pensize(5)
+    jess.penup()
+    jess.setpos(80, -65)
+    jess.pendown()
+    for j in range(1):
+        jess.forward(150)
+        jess.left(145)
+        jess.forward(90)
+        jess.left(70)
+        jess.forward(90)
+
+def function_5():
+    """ creates star
+        :param : turtle object"""
+    willy.color("sky blue")
+    willy.pensize(1)
+    willy.penup()
+    willy.setpos(250, 100)
+    willy.pendown()
+    willy.begin_fill()
+    for t in range(5):
+        willy.forward(90)
+        willy.left(145)
+    willy.end_fill()
+    willy.hideturtle()
 
 def main():
     function_1()
     function_2()
     function_3()
+    function_4()
+    function_5()
 
     wn.exitonclick()
 #coment coment
