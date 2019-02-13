22999de5d342d5debf68feeba289afb6f052d419
chemputer <46601333+chemputer@users.noreply.github.com>
2019-02-13 17:49:09 -0500

    Added docstrings, and made a few minor changes, added a settings area. More comments.

--- a03-turnerb.py
+++ a03-turnerb.py
@@ -3,9 +3,10 @@
 # A03: A Pair of Fully Functional Gitty Psychedelic Robotic Turtles
 # references the assignment doc:
 # https://docs.google.com/document/d/18mGsKUgGGTHoyN-BFd13Pg0GE5MTkCofZjxaS1oXSR0/edit?usp=sharing
-# inspiration and ideas from the following sources:
-# Fibonacci sequence in python using Turtle - https://trinket.io/python/43bc79b582
-# Generating a fibonacci sequence in python - https://mortada.net/fibonacci-numbers-in-python.html
+#
+# sources used:
+# [1] Fibonacci sequence in python using Turtle - https://trinket.io/python/43bc79b582
+# [2] Generating a fibonacci sequence in python - https://mortada.net/fibonacci-numbers-in-python.html
 #
 ##
 
@@ -15,12 +16,26 @@ from random import randint
 # initialize the turtle t and screen wn
 t = turtle.Turtle()
 wn = turtle.Screen()
+
+# settings
+
+# changes the factor of the side length
+f = 1
+# speed of the turtle
+turtleSpeed = 0
+
+# settings for the colors - boxes, background, and spiral
+colors = ["purple", "yellow", "green"]
 # set background color to yellow
-wn.bgcolor("yellow")
+
+# end settings
+t.speed(turtleSpeed)
+wn.bgcolor(colors[1])
 
 
 def rec_fib(n):
-    # recursive algorithm to generate a fibonacci sequence
+    """a recursive algorithm to generate a fibonacci sequence -- taken almost directly from [2]"""
+
     if n == 0:  # when n is 0, the next number has to be 0.
         return 0
     elif n == 1:  # when n is 1, the next number is 1
@@ -30,18 +45,21 @@ def rec_fib(n):
 
 
 # generate a fib sequence with a length of 30
-fibseq = ([rec_fib(i) for i in range(randint(10, 20))])
+fib_seq = ([rec_fib(i) for i in range(randint(10, 20))])
+# length of the generated fib sequence
+num = len(fib_seq)
 
 
-def dwsquare(tr, side_len, pencolor):
-    # draw a square using turtle tr, with a side length of side_len
+def drawsquare(tr, side_len, pencolor):
+    """draw a square using turtle tr, with a side length of side_len"""
     tr.pencolor(pencolor)
     for s in range(4):
         tr.forward(side_len)
         tr.right(90)
 
+
 def drawfib(tr):
-    # draw the fibonnaci sequence using squares with a side length equal to the fibonacci numbers
+    """draw the fibonnaci sequence using squares with a side length equal to the fibonacci numbers"""
 
     # make sure the pen is up
     tr.penup()
@@ -52,41 +70,45 @@ def drawfib(tr):
 
     # iterate through the fib sequence, and draw the square with the length of that number
     for i in range(num):
-        dwsquare(tr, n*fibseq[i], "purple")
+        # draw a square with turtle, length of the fib sequence of index i, in the color purple
+        drawsquare(tr, f * fib_seq[i], colors[0])
         tr.penup()
-        tr.fd(n * fibseq[i])
+        # move forward the same amount, turn 90 degrees to the right, and move forward again
+        tr.fd(f * fib_seq[i])
         tr.right(90)
-        tr.fd(n * fibseq[i])
+        tr.fd(f * fib_seq[i])
         tr.pendown()
     # set the penup since we're done with it now.
     tr.penup()
 
 
 def drawcir(tr):
-    # draws a series of quarter circles, showing the fibonnaci spiral, using the turtle tr
+    """draws a series of quarter circles, showing the fibonnaci spiral, using the turtle tr"""
 
     tr.penup()
+
+    # move the turtle to quadrant A, so it's off center a little bit, at coordinates 50, 50
     tr.goto(50, 50)
+    # set the heading to 0
     tr.seth(0)
-    tr.pencolor('green')
+    # change the pencolor to green
+    tr.pencolor(colors[2])
+    # increase the pensize a bit
     tr.pensize(5)
     tr.pendown()
-
+    # draw 90 degrees (a quarter) of a circle, with the radius of the number in the fib sequence
+    # multiplied by a negative factor, so it draws in the clockwise direction
     for i in range(num):
-        tr.circle(-n*fibseq[i], 90)
-
-# initialize constants
-num = len(fibseq)
-# changes the factor of the side length
-n = 1
-# speed of the turtle
-t.speed(0)
+        tr.circle(-f * fib_seq[i], 90)
 
 
 def main():
-    # main method, lets us run all the methods at once
+    """main method, lets us run all the methods at once, in the correct order"""
     drawfib(t)
     drawcir(t)
+    # keeps the screen open once finished, so that it doesn't close unless you close it
     turtle.done()
 
+
+# run the main method
 main()
